e8b7eb5fb92589149142040025a19c35d9f81b17
Tang Tang <tang6723@hotmail.com>
2016-11-04 22:50:05 +0800

    tang

--- src/pages/home/popover/popover.ts
+++ src/pages/home/popover/popover.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, ViewController} from 'ionic-angular';
+
+import {Login} from '../../login/login';
+import {SettingHome} from '../../setting/setting-home/setting-home';
+import {AppGlobal} from '../../../providers/app-global';
+import {HomePage} from '../../home/home';
 
 /*
   Generated class for the Popover page.
@@ -12,11 +17,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'popover.html'
 })
 export class Popover {
+  settingHome=SettingHome;
+  appInstance:AppGlobal;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public viewCtrl: ViewController,public navCtrl: NavController) {
+    this.appInstance=AppGlobal.getInstance();
+  }
 
   ionViewDidLoad() {
     console.log('Hello Popover Page');
+  }
+
+  pushSettingHome() {
+    this.viewCtrl.dismiss();
+    this.navCtrl.setRoot(HomePage);
+    this.navCtrl.push(this.settingHome);
+  }
+
+  logOut(){
+    this.appInstance.username="";
+    this.appInstance.usertitle="";
+    this.viewCtrl.dismiss();
+    this.navCtrl.setRoot(Login);
   }
 
 }
