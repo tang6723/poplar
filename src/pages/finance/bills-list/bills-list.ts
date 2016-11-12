9dfb06d1e59d35bab49ae523e488b2c82452b345
Tang Tang <tang6723@hotmail.com>
2016-11-12 22:51:54 +0800

    purchase

--- src/pages/finance/bills-list/bills-list.ts
+++ src/pages/finance/bills-list/bills-list.ts
@@ -24,51 +24,51 @@ export class BillsList {
   billTime4:string="2016年6月";
 
   contractitems1: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201609040008', dateissue: '09-04', billstate: '草案',total: 30080  },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201609040007', dateissue: '09-04', billstate: '执行',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201609040006', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201609040005', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201609040004', dateissue: '09-04', billstate: '结账',total: 30080  }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '鞍山矿业集团有限公司',billno: '#201609040008', dateissue: '09-04', billstate: '草案',total: 30080  },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '鞍山公交公司',billno: '#201609040007', dateissue: '09-04', billstate: '执行',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '鞍山一中',billno: '#201609040006', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '鞍山矿业集团有限公司',billno: '#201609040005', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '鞍山一中',billno: '#201609040004', dateissue: '09-04', billstate: '结账',total: 30080  }
   ];
 
   contractitems2: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '鞍山一中',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '鞍山矿业集团有限公司',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '鞍山公交公司',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '鞍山矿业集团有限公司',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '鞍山一中',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
   ];
 
   orderitems1: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201609040008', dateissue: '09-04', billstate: '草案',total: 30080  },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201609040007', dateissue: '09-04', billstate: '执行',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201609040006', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201609040005', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201609040004', dateissue: '09-04', billstate: '结账',total: 30080  }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '江苏木业公司',billno: '#201609040008', dateissue: '09-04', billstate: '草案',total: 30080  },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '王老吉凉茶叶',billno: '#201609040007', dateissue: '09-04', billstate: '执行',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '木林森集团',billno: '#201609040006', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '木林森集团',billno: '#201609040005', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '王老吉凉茶叶',billno: '#201609040004', dateissue: '09-04', billstate: '结账',total: 30080  }
   ];
 
   orderitems2: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '王老吉凉茶叶',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '江苏木业公司',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '王老吉凉茶叶',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '木林森集团',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '江苏木业公司',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
   ];
 
   expenseitems1: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201609040008', dateissue: '09-04', billstate: '草案',total: 30080  },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201609040007', dateissue: '09-04', billstate: '执行',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201609040006', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201609040005', dateissue: '09-04', billstate: '审核',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201609040004', dateissue: '09-04', billstate: '结账',total: 30080  }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '王宝强',billno: '出差报销', dateissue: '09-04', billstate: '草案',total: 30080  },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '微小宝',billno: '水电费', dateissue: '09-04', billstate: '执行',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '马蓉',billno: '出差报销', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '王宝强',billno: '加班费', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '微小宝',billno: '水电费', dateissue: '09-04', billstate: '结账',total: 30080  }
   ];
 
   expenseitems2: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '微小宝',billno: '加班费', dateissue: '08-04', billstate: '草案',total: 30080 },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '王宝强',billno: '房租', dateissue: '08-04', billstate: '执行',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '马蓉',billno: '出差报销', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '王宝强',billno: '加班费', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '微小宝',billno: '水电费', dateissue: '08-04', billstate: '结账',total: 30080 }
   ];
 
   raiseitems1: any = [
--- src/pages/sales/contract-list/contract-list.ts
+++ src/pages/sales/contract-list/contract-list.ts
@@ -23,19 +23,19 @@ export class ContractList {
   items: string[];
 
   items1: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#160904008', dateissue: '09-04', billstate: '草案',total: 30080  },
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '鞍山矿业集团有限公司',billno: '#160904008', dateissue: '09-04', billstate: '草案',total: 30080  },
     {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#160904007', dateissue: '09-04', billstate: '执行',total: 30080  },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#160904006', dateissue: '09-04', billstate: '审核',total: 30080  },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '鞍山矿业集团有限公司',billno: '#160904006', dateissue: '09-04', billstate: '审核',total: 30080  },
     {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#160904005', dateissue: '09-04', billstate: '审核',total: 30080  },
     {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#160904004', dateissue: '09-04', billstate: '结账',total: 30080  }
   ];
 
   items2: any = [
     {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#160804008', dateissue: '08-04', billstate: '草案',total: 30080 },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#160804007', dateissue: '08-04', billstate: '执行',total: 30080 },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '鞍山矿业集团有限公司',billno: '#160804007', dateissue: '08-04', billstate: '执行',total: 30080 },
     {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#160804006', dateissue: '08-04', billstate: '审核',total: 30080 },
     {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#160804005', dateissue: '08-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#160804004', dateissue: '08-04', billstate: '结账',total: 30080 }
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '鞍山矿业集团有限公司',billno: '#160804004', dateissue: '08-04', billstate: '结账',total: 30080 }
   ];
 
   constructor(public navCtrl: NavController) {}
