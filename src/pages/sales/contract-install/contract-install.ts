844d9e0a79a259583025212c5e2480f4852de799
Tang Tang <tang6723@hotmail.com>
2016-11-13 10:19:17 +0800

    finance

--- src/pages/sales/contract-install/contract-install.ts
+++ src/pages/sales/contract-install/contract-install.ts
@@ -13,6 +13,12 @@ import {NavController, ViewController} from 'ionic-angular';
 })
 export class ContractInstall {
 
+  public event = {
+    month: '1990-02-19',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'
+  }
+
   constructor(public navCtrl: NavController,public viewCtrl: ViewController) {}
 
   ionViewDidLoad() {
