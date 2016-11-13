1e4705cbb762efa969cbd6e9ab2c03e216eb645d
Tang Tang <tang6723@hotmail.com>
2016-11-13 10:38:41 +0800

    finance

--- src/pages/purchase/order-storage/order-storage.ts
+++ src/pages/purchase/order-storage/order-storage.ts
@@ -13,6 +13,18 @@ import {NavController, ViewController} from 'ionic-angular';
 })
 export class OrderStorage {
 
+  billid:string="#201609040008";
+  billstate:string="草案";
+  billtotal:number =30380.00;
+
+  items: any = [
+    {imgurl: 'ios-flag-outline',id: '123456', name: 'Apple MacBook Air 13.3英寸笔记本电脑 ', price: 200.00, num: 3, specification: '规格13.3英寸笔记本'},
+    {imgurl: 'ios-flame-outline',id: '123457', name: 'Apple MacBook Air ', price: 1120.00, num: 9, specification: '规格13.3英寸笔记本'},
+    {imgurl: 'ios-flash-outline',id: '123458', name: '商品3 Apple MacBook Air ', price: 280.00, num: 6, specification: '规格13.3英寸笔记本'},
+    {imgurl: 'ios-flash-outline',id: '123459', name: 'Apple MacBook Air ', price: 2450.23, num: 15, specification: '规格13.3英寸笔记本'},
+    {imgurl: 'ios-flash-outline',id: '123460', name: 'Apple MacBook Air ', price: 900, num: 30, specification: '规格13.3英寸笔记本'}
+  ];
+
   constructor(public navCtrl: NavController,public viewCtrl: ViewController) {}
 
   ionViewDidLoad() {
