b15ebe545b49637acdc1ec7057e1483ae657baea
tang6723 <tang6723@hotmai.com>
2016-11-18 14:29:46 +0800

    HProse read

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, AlertController, LoadingController} from 'ionic-angular';
 
 import {HomePage} from '../home/home';
@@ -6,13 +6,12 @@ import {AppGlobal} from '../../providers/app-global'
 import {UserData, SystemUser} from '../../providers/user-data';
 
 
-
 /*
-  Generated class for the Login page.
+ Generated class for the Login page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -24,129 +23,107 @@ export class Login {
   userName: string = "1";
   passWord: string = "1";
 
-  userRole:string="";
-  isBill:boolean=false;
-  isCheck:boolean=false;
-  isCharging:boolean=false;
-  isStock:boolean=false;
-  isTraffic:boolean=false;
-  isInstall:boolean=false;
-  isRaise:boolean=false;
-  isSetting:boolean=false;
-  appInstance:AppGlobal;
-  message: string = "Ice cream. It's Good and You Want It.";
+  userRole: string = "";
+  isBill: boolean = false;
+  isCheck: boolean = false;
+  isCharging: boolean = false;
+  isStock: boolean = false;
+  isTraffic: boolean = false;
+  isInstall: boolean = false;
+  isRaise: boolean = false;
+  isSetting: boolean = false;
+  appInstance: AppGlobal;
+  //message: string = "Ice cream. It's Good and You Want It.";
   userInfo: SystemUser;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController,public userDataService:UserData,public loadingCtrl: LoadingController) {
-    this.appInstance=AppGlobal.getInstance();
-    this.userDataService.getHelloWord().then(data=>{
-      console.log(data);
-      this.userName=data;
-      this.userCode=data.userName;
-      console.log(this.userCode);
-    });
-  }
-  onPageWillEnter() {
-    // 在这里可以做页面初始化的一些事情
-    console.log('page 1: page will enter.');
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public userDataService: UserData, public loadingCtrl: LoadingController) {
+    this.appInstance = AppGlobal.getInstance();
+
   }
 
   ionViewDidLoad() {
-
-    /*console.log('Hello Login Page');
-    //console.log(this.userName);
-    console.time("hprose");
-    //this.userDataService.getHelloWord().then(data=> {
-    //   this.userInfo=data;
-    //   this.userCode=data.userName;
-
-    // });
-    //this.userInfo=this.userDataService.getHelloWord();
-    //this.userInfo=this.userDataService.getIceCream();
-    //this.userCode ='13';
-    console.log(this.userInfo);
-    console.timeEnd("hprose");
-    console.log('Hello Login Page1');
-    //this.message=this.userDataService.getIceCream();
-    //console.log(this.message);*/
-  }
-  getHeroes(): void {
-    //this.userDataService.getHelloWord().then(heroes => this.userName = heroes);
-    this.userName=this.userDataService.getHelloWord();
+    console.log('Hello Login Page');
+    this.userDataService.getHelloWord().then(data=> {
+      console.log(data);
+      this.userName = data;
+      this.userCode = data.userName;
+      console.log(this.userCode);
+    });
+    this.presentLoading();
   }
 
   loginClick() {
     let err: string = "";
-    if(this.passWord=="1" && this.userCode=="1"){
-      this.userName="王宝强";
-      this.userRole="员工";
-      this.isBill=true;
-      this.isCheck=false;
-      this.isCharging=false;
-      this.isStock=true;
-      this.isTraffic=true;
-      this.isInstall=true;
-      this.isRaise=false;
-      this.isSetting=false;
+    if (this.passWord == "1" && this.userCode == "1") {
+      this.userName = "王宝强";
+      this.userRole = "员工";
+      this.isBill = true;
+      this.isCheck = false;
+      this.isCharging = false;
+      this.isStock = true;
+      this.isTraffic = true;
+      this.isInstall = true;
+      this.isRaise = false;
+      this.isSetting = false;
 
-      this.appInstance.username=this.userName;
-      this.appInstance.usertitle=this.userRole;
-      this.appInstance.isBill=this.isBill;
-      this.appInstance.isCheck=this.isCheck;
-      this.appInstance.isCharging=this.isCharging;
-      this.appInstance.isStock=this.isStock;
-      this.appInstance.isTraffic=this.isTraffic;
-      this.appInstance.isInstall=this.isInstall;
-      this.appInstance.isRaise=this.isRaise;
-      this.appInstance.isSetting=this.isSetting;
+      this.appInstance.username = this.userName;
+      this.appInstance.usertitle = this.userRole;
+      this.appInstance.isBill = this.isBill;
+      this.appInstance.isCheck = this.isCheck;
+      this.appInstance.isCharging = this.isCharging;
+      this.appInstance.isStock = this.isStock;
+      this.appInstance.isTraffic = this.isTraffic;
+      this.appInstance.isInstall = this.isInstall;
+      this.appInstance.isRaise = this.isRaise;
+      this.appInstance.isSetting = this.isSetting;
 
       this.navCtrl.setRoot(HomePage);
 
-    }else if(this.passWord=="1" && this.userCode=="2"){
-      this.userName="马容";
-      this.userRole="经理";
-      this.isBill=false;
-      this.isCheck=true;
-      this.isCharging=true;
-      this.isStock=true;
-      this.isTraffic=true;
-      this.isInstall=true;
-      this.isRaise=true;
-      this.isSetting=true;
+    } else if (this.passWord == "1" && this.userCode == "2") {
+      this.userName = "马容";
+      this.userRole = "经理";
+      this.isBill = false;
+      this.isCheck = true;
+      this.isCharging = true;
+      this.isStock = true;
+      this.isTraffic = true;
+      this.isInstall = true;
+      this.isRaise = true;
+      this.isSetting = true;
 
-      this.appInstance.username=this.userName;
-      this.appInstance.usertitle=this.userRole;
-      this.appInstance.isBill=this.isBill;
-      this.appInstance.isCheck=this.isCheck;
-      this.appInstance.isCharging=this.isCharging;
-      this.appInstance.isStock=this.isStock;
-      this.appInstance.isTraffic=this.isTraffic;
-      this.appInstance.isInstall=this.isInstall;
-      this.appInstance.isRaise=this.isRaise;
-      this.appInstance.isSetting=this.isSetting;
+      this.appInstance.username = this.userName;
+      this.appInstance.usertitle = this.userRole;
+      this.appInstance.isBill = this.isBill;
+      this.appInstance.isCheck = this.isCheck;
+      this.appInstance.isCharging = this.isCharging;
+      this.appInstance.isStock = this.isStock;
+      this.appInstance.isTraffic = this.isTraffic;
+      this.appInstance.isInstall = this.isInstall;
+      this.appInstance.isRaise = this.isRaise;
+      this.appInstance.isSetting = this.isSetting;
       this.navCtrl.setRoot(HomePage);
 
-    }else if(this.passWord=="1" && this.userCode=="3"){
-      this.userRole="财务";
-      this.isBill=true;
-      this.isCheck=true;
-      this.isCharging=true;
-      this.isStock=true;
-      this.isTraffic=true;
-      this.isInstall=true;
-      this.isRaise=true;
-      this.isSetting=true;
+    } else if (this.passWord == "1" && this.userCode == "3") {
+      this.userRole = "财务";
+      this.isBill = true;
+      this.isCheck = true;
+      this.isCharging = true;
+      this.isStock = true;
+      this.isTraffic = true;
+      this.isInstall = true;
+      this.isRaise = true;
+      this.isSetting = true;
 
-      this.appInstance.username=this.userName;
-      this.appInstance.usertitle=this.userRole;
-      this.appInstance.isBill=this.isBill;
-      this.appInstance.isCheck=this.isCheck;
-      this.appInstance.isCharging=this.isCharging;
-      this.appInstance.isStock=this.isStock;
-      this.appInstance.isTraffic=this.isTraffic;
-      this.appInstance.isInstall=this.isInstall;
-      this.appInstance.isRaise=this.isRaise;
-      this.appInstance.isSetting=this.isSetting;
+      this.appInstance.username = this.userName;
+      this.appInstance.usertitle = this.userRole;
+      this.appInstance.isBill = this.isBill;
+      this.appInstance.isCheck = this.isCheck;
+      this.appInstance.isCharging = this.isCharging;
+      this.appInstance.isStock = this.isStock;
+      this.appInstance.isTraffic = this.isTraffic;
+      this.appInstance.isInstall = this.isInstall;
+      this.appInstance.isRaise = this.isRaise;
+      this.appInstance.isSetting = this.isSetting;
       this.navCtrl.setRoot(HomePage);
 
     }
@@ -165,31 +142,20 @@ export class Login {
 
   }
 
-  passwordClick()
-  {
+  passwordClick() {
     //this.userCode=this.userInfo['userName'];
     this.userDataService.getHelloWord().then(data=> {
-      this.userInfo=data;
-      this.userCode=data.userName;
+      this.userInfo = data;
+      this.userCode = data.userName;
       alert(this.userCode);
 
     });
   }
 
-  ngAfterViewInit(){
-    /*this.userDataService.getHelloWord().then(data=> {
-      this.userInfo=data;
-      this.userCode=data.userName;
-      alert(this.userCode);
-
-    });
-    console.log('Hello Login Page ngAfterViewInit');*/
-    this.presentLoading();
-  }
   presentLoading() {
     let loading = this.loadingCtrl.create({
       content: "Please wait...",
-      duration: 3000,
+      duration: 2000,
       dismissOnPageChange: true
     });
     loading.present();
