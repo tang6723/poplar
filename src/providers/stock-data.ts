394a68270193221455a6b92f4e4e4d91946c6c08
Tang Tang <tang6723@hotmail.com>
2016-11-19 00:23:11 +0800

    zhang

--- src/providers/stock-data.ts
+++ src/providers/stock-data.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+declare var hprose;
+
 /*
   Generated class for the StockData provider.
 
@@ -9,10 +11,49 @@ import 'rxjs/add/operator/map';
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
+export class StockDataProperty{
+  StockDataID	:number;
+  RowGUID	:string;
+  EnterpriseCode	:string;
+  EnterpriseName	:string;
+  OrganizationCode	:string;
+  OrganizationName	:string;
+  StockID	:string;
+  MaterialID	:string;
+  CostOrganizationCode	:string;
+  AccountYear	:string;
+  BatchNo	:string;
+  OpeningStock	:number;
+  WarehouseQty	:number;
+  OutputQty	:number;
+  EndingQty	:number;
+  OpeningAmount	:number;
+  State	:string;
+  Remarks	:string;
+
+};
+
+
+
+@Injectable()
 export class StockData {
 
   constructor(public http: Http) {
     console.log('Hello StockData Provider');
+  }
+
+  getStockDataList(){
+    console.log("getStockDataList Start!!");
+    var client = hprose.Client.create("http://localhost:8080/DataService/DataService", ["getStockDataList"]);
+    var result;
+    client.getStockDataList().then(function (d) {
+      console.error(d);
+      result = d;
+      console.log("getStockDataList End!!");
+      // return d;
+    });
+    console.error(result);
+    return result;
   }
 
 }
